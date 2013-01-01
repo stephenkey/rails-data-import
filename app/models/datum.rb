27d1e26fb12dadca9338cd49e0fc77657a574d06
Stephen Key <Stephenkey123@gmail.com>
2013-01-01 11:10:54 -0600

    Fixing indent

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -2,13 +2,13 @@ class Datum < ActiveRecord::Base
   attr_accessible :title, :quantity, :condition, :description
 
   # A method to check if the file is an acceptable format and use the 'roo' gem to open the file.
-	def self.open_spreadsheet(file)
-	  case File.extname(file.original_filename)
-		  when ".csv" then Csv.new(file.path, nil, :ignore)
-		  when ".xls" then Excel.new(file.path, nil, :ignore)
-		  when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-		  else raise "Unknown file type: #{file.original_filename}"
-	  end
-	end
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+      when ".csv" then Csv.new(file.path, nil, :ignore)
+      when ".xls" then Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+      else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
 
 end
